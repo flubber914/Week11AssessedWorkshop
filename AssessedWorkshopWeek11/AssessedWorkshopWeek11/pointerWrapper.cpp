37f8dfbdea8e65e38589042be55209c7a5ce528c
flubber914 <117917092+flubber914@users.noreply.github.com>
2023-12-06 00:57:34 +0000

    Add files via upload

--- AssessedWorkshopWeek11/AssessedWorkshopWeek11/pointerWrapper.cpp
+++ AssessedWorkshopWeek11/AssessedWorkshopWeek11/pointerWrapper.cpp
@@ -1,16 +1,28 @@
 #include "pointerWrapper.h"
 
+/// <summary>
+/// print the smart pointer value
+/// </summary>
+/// <param name=""></param>
 void pointerWrapper::print(void)
 {
 	cout << "Hi, I am a wrapped pointer with value: " << *p << endl;
 }
 
+/// <summary>
+/// Constructor:
+/// parse in integer and set unique pointer using make_unique to avoid the 'new' keyword
+/// </summary>
+/// <param name="_p"></param>
 pointerWrapper::pointerWrapper(int _p)
 {
 	cout << "Allocating memory for int pointer" << endl;
 	p = make_unique<int>(_p);
 }
 
+/// <summary>
+/// call the destructor and let the smart pointer delete the memory
+/// </summary>
 pointerWrapper::~pointerWrapper()
 {
 	cout << "Releasing memory for int pointer" << endl;
